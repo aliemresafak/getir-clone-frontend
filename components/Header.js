4dce5c774802a7b7271753efdbc944cd06a96719
Ali Emre Şafak <aliemresafak@gmail.com>
2021-04-07 00:55:30 +0300

    Initialized project

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#">getir</a>
+                    <a href="#">getiryemek</a>
+                    <a href="#">getirbüyük</a>
+                </div>
+                <div className="right">
+                    <a href="#">Giriş Yap</a>
+                    <a href="#">Kayıt Ol</a>
+                </div>
+            </div>
+        </header>
+
+    )
+}
